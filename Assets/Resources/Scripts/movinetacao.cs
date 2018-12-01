3677cc5400c9440ce024adcb124e8c4b9332a5b3
camarada_ramon <ramonboucascp2@gmail.com>
2018-11-30 23:48:36 -0200

    scripts dos estados
    esses sao os scripts dos estados do player de acordo com o diagrama de classes (diagrama de estados na forma de classes)

---
+++ Assets/Resources/Scripts/movinetacao.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movinetacao : MonoBehaviour {
+
+   public int moduloForca = 5;
+   public  Rigidbody2D euMesmo;
+
+	// Use this for initialization
+	void Start () {
+		
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            euMesmo.AddForce(new Vector2(-1 * moduloForca,0));
+        }
+
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            euMesmo.AddForce(new Vector2(moduloForca, 0));
+        }
+
+        // codigo relativo ao estado solido
+
+        euMesmo.gravityScale = 2;
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Assets/Resources/Scripts/state.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class state {
+
+	 
+	 
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void firstFrame()
+    {
+
+    }
+    void lastFrame()
+    {
+
+    }
+}
