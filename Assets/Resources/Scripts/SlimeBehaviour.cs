093c6bc165631b69e99da2ac0196ce88510dc4cd
Gabriel de Souza <devgabrieldesouza@gmail.com>
2018-12-08 23:52:25 -0200

    Horizontal Raycasts
    Horizontal wall detectors. Some stuff removed from the movement script.

--- Assets/Resources/Scripts/SlimeBehaviour.cs
+++ Assets/Resources/Scripts/SlimeBehaviour.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class SlimeBehaviour : MonoBehaviour {
 
+
     [SerializeField]
     private float speed;
 
+    #region Detectors Settings
     [SerializeField]
-    [Range(0,1)]
+    private float floorOffset;
+
+    [SerializeField]
+    [Range(0, 1)]
     private float detectorOffset;
+
+    [SerializeField]
+    [Range(0, 1)]
+    private float detectorTopOffset;
 
     [SerializeField]
     private float detectorDirection;
@@ -19,15 +28,24 @@ public class SlimeBehaviour : MonoBehaviour {
     private float detectorDistance;
 
     [SerializeField]
+    [Range(0, 10)]
+    private float detectorTopDistance;
+
+    [SerializeField]
     private LayerMask detectorMask;
+    #endregion
 
-
+    #region Detectors Variables
     private FloorDetector leftDetector;
+
+    private FloorDetector leftTopDetector;
 
     private FloorDetector rightDetector;
 
-    private FloorDetector middleDetector;
+    private FloorDetector rightTopDetector;
 
+    private FloorDetector middleDetector;
+    #endregion
 
     private Rigidbody2D rb;
    
@@ -47,63 +65,59 @@ public class SlimeBehaviour : MonoBehaviour {
 
     void FixedUpdate()
     {
-        float inputDirection = Input.GetAxisRaw("Horizontal");
-
-        rb.velocity = new Vector2(inputDirection * speed, rb.velocity.y);
-
-        if (leftDetector.Hit && rightDetector.Hit)
-        {
-
-            //print((leftDetector.Hit.point + rightDetector.Hit.point) / 2);
-
-            rb.position = ((leftDetector.Hit.point + rightDetector.Hit.point)/2) + (Vector2.up * 0.5f);
-        }
+        
     }
 
     void OnDrawGizmos(){
-        // Draws a 5 unit long red line in front of the object
         Gizmos.color = Color.yellow;
 
         InitializeDetectors();
 
         Gizmos.DrawLine(leftDetector.Origin, leftDetector.Target);
 
+        Gizmos.DrawLine(leftTopDetector.Origin, leftTopDetector.Target);
+
+
         Gizmos.DrawLine(rightDetector.Origin, rightDetector.Target);
+
+        Gizmos.DrawLine(rightTopDetector.Origin, rightTopDetector.Target);
+
 
         Gizmos.DrawLine(middleDetector.Origin, middleDetector.Target);
     }
 
     private void InitializeDetectors(){
 
+        middleDetector = new FloorDetector(transform.position, transform.localRotation * Vector2.down, detectorDistance, detectorMask);
+
+
         leftDetector = new FloorDetector(transform.TransformPoint(Vector3.left * detectorOffset), transform.localRotation * new Vector2(detectorDirection, -1), detectorDistance, detectorMask);
+
+        leftTopDetector = new FloorDetector(transform.TransformPoint(Vector3.left * detectorTopOffset), transform.localRotation * Vector2.left, detectorTopDistance, detectorMask);
+
 
         rightDetector = new FloorDetector(transform.TransformPoint(Vector3.right * detectorOffset), transform.localRotation * new Vector2(-detectorDirection, -1), detectorDistance, detectorMask);
 
-        middleDetector = new FloorDetector(transform.position, transform.localRotation * Vector2.down, detectorDistance, detectorMask);
+        rightTopDetector = new FloorDetector(transform.TransformPoint(Vector3.right * detectorTopOffset), transform.localRotation * Vector2.right, detectorTopDistance, detectorMask);
     }
 
     private void UpdateDetectors(){
         middleDetector.Origin = transform.position;
         middleDetector.Direction = transform.localRotation * Vector2.down;
 
+
         leftDetector.Origin = transform.TransformPoint(Vector3.left * detectorOffset);
         leftDetector.Direction = transform.localRotation * new Vector2(detectorDirection, -1);
+        
+        leftTopDetector.Origin = transform.TransformPoint(Vector3.left * detectorTopOffset);
+        leftDetector.Direction = transform.localRotation * Vector2.left;
+
 
         rightDetector.Origin = transform.TransformPoint(Vector3.right * detectorOffset);
         rightDetector.Direction = transform.localRotation * new Vector2(-detectorDirection, -1);
+
+        rightTopDetector.Origin = transform.TransformPoint(Vector3.right * detectorTopOffset);
+        rightDetector.Direction = transform.localRotation *Vector2.right;
     }
-
-    /*bool doubleRaycastDown(TerrainMovementRayProperties movementRay, float rayLength, out RaycastHit leftHitInfo, out RaycastHit rightHitInfo){
-        Vector3 transformUp = transform.up;
-        Vector3 transformRight = transform.right;
-
-        Ray leftRay = new Ray(transform.position + movementRay.originOffsetY * transformUp + movementRay.distanceFromCenter * transformRight, -transformUp);
-        Ray rightRay = new Ray(transform.position + movementRay.originOffsetY * transformUp - movementRay.distanceFromCenter * transformRight, -transformUp);
-
-        //Ray leftRay = new Ray(transform.position + leftOffset * transformUp + leftOffset * transformRight, -transformUp);
-        //Ray rightRay = new Ray(transform.position + rightOffset * transformUp - rightOffset * transformRight, -transformUp);
-
-        return Physics.Raycast(leftRay, out leftHitInfo, rayLength, DefaultTerrainLayerMask) && Physics.Raycast(rightRay, out rightHitInfo, rayLength, DefaultTerrainLayerMask);
-    }*/
 
 }
