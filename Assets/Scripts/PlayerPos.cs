51f8e456a5050950f1cba0ca7e1dd4c10c60afea
Nicholas <nickvilla@hotmail.com.br>
2018-12-14 20:02:05 -0200

    MenuPause
    MenuPause

---
+++ Assets/Scripts/PlayerPos.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+                                                //COLOCAR NO PERSONAGEM COM TAG DE "Player" para salvar a posição
+public class PlayerPos : MonoBehaviour {
+
+    private GameMaster gm;
+    private bool isDead;
+
+    void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        transform.position = gm.lastCheckPointPos;
+    }
+
+    //IDEIA DE PARA QUANDO A VARIAVEL QUE DEVE SER CHAMADA NO SCRIPT FOR TRUE DE O PLAYER TER MORRIDO, ELE RECEBE A POSIÇÃO DO ULTIMO CHECKPOINT
+    void Update()
+    {
+       if(isDead = true)
+        {
+            transform.position = gm.lastCheckPointPos;
+        }
+    }
+}
