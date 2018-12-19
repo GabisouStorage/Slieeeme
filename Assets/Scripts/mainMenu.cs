125d4b9687501271e37472208b3240511ad7c1cb
Gabriel de Souza <devgabrieldesouza@gmail.com>
2018-12-19 20:59:48 -0200

    Level Update 1

--- Assets/Scripts/mainMenu.cs
+++ Assets/Scripts/mainMenu.cs
@@ -24,7 +24,7 @@ public class mainMenu : MonoBehaviour
     public void jogar()
     {
         Debug.Log("play");
-        SceneManager.LoadScene("level1");
+        SceneManager.LoadScene("Level1");
        sfxMan.play.Play();
 
     }
