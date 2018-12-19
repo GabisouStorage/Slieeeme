32f5b904e8afaae9a530125814ec743fe82be78c
camarada_ramon <ramonboucascp2@gmail.com>
2018-12-19 17:55:24 -0200

    organizaçao dos arquivos

---
+++ Assets/Resources/Scripts/botaoController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class botaoController : MonoBehaviour {
+
+    public bool estouPressionado = false;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        pressionarBotao();
+    }
+
+
+    public void pressionarBotao()
+    {
+        estouPressionado = true;
+
+    }
+
+
+
+
+
+
+}
--- Assets/Resources/Scripts/evilSlimeController.cs
+++ Assets/Resources/Scripts/evilSlimeController.cs
@@ -5,24 +5,45 @@ using UnityEngine;
 public class evilSlimeController : MonoBehaviour {
 
 
+    
+ 
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-
-        if ((collision.collider.tag == "Player") && stateController.estadoAtual is slime)
+        if (collision.collider.tag == "Player") 
         {
+             if (stateController.estadoAtual is slime) { 
+                matarPlayer();
+                morrer();
+             }
 
-            stateController.mortePlayer();
+            if (stateController.estadoAtual is solido)
+            {
+                Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), collision.collider);
+            }
 
+            if (stateController.estadoAtual is gasoso)
+            {
+                Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), collision.collider);
+            }
         }
-
-
-
-
     }
 
 
 
 
+    public void morrer()
+    {
+        Destroy(gameObject);
 
+    }
+
+    public void matarPlayer()
+    {
+        stateController.mortePlayer();
+
+    }
+
+ 
 
 }
